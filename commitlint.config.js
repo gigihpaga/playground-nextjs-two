bc86ca7611c1b7c8b0bf29d7d5761ac42d462ad6
Gigih Patga <gigihpatga@gmail.com>
2024-06-09 20:56:22 +0700

    style(format): change indent file config
    deskripsi:
    * mengubah indentation tab file config menjadi 4

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [2, 'always', 251],
-    'scope-case': [2, 'always', 'kebab-case'],
-    'body-max-line-length': [2, 'always', 509], // caracter in body (per line)
-    'body-max-length': [2, 'always', 5015], // total caracter in body
-    'footer-max-line-length': [2, 'always', 555], // caracter in footer (per line)
-  },
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 251],
+        'scope-case': [2, 'always', 'kebab-case'],
+        'body-max-line-length': [2, 'always', 509], // caracter in body (per line)
+        'body-max-length': [2, 'always', 5015], // total caracter in body
+        'footer-max-line-length': [2, 'always', 555], // caracter in footer (per line)
+    },
 };

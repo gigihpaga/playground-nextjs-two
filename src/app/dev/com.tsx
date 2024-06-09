91e4cb732d747b7457cbaf5225375da1df031006
Gigih Patga <gigihpatga@gmail.com>
2024-06-09 19:00:09 +0700

    chore(lint-pre-commit): testing commit after setup husky
    deskripsi:
    percobaan commit yang sukses

---
+++ src/app/dev/com.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+// import Image from 'next/image';
+
+export default function com() {
+    const a: Array<string | undefined> = [];
+    console.log(a);
+    // a = 'paga';
+    // a = 1;
+    return (
+        <div>
+            com
+            {/* <Image /> */}
+            <h1>da</h1>
+        </div>
+    );
+}

a5742e77aa25cf0df2b4513b4ea1948aa92e93b2
Gigih Patga <gigihpatga@gmail.com>
2024-06-28 23:33:53 +0700

    refactor: prisma enable logger
    deskripsi:
    * mengaktifkan query logger prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, type Prisma } from '@prisma/client';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+const log: Prisma.PrismaClientOptions['log'] | undefined = isDevelopment
+    ? [
+          {
+              emit: 'stdout',
+              level: 'query',
+          },
+      ]
+    : undefined;
 
 const prismaClientSingleton = () => {
-    return new PrismaClient();
+    return new PrismaClient({
+        // commented this "log" for non-active logger
+        log: log,
+    });
 };
 
 declare const globalThis: {

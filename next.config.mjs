272d449ebc9106d984c849fa005e4bab1278b636
Gigih Patga <gigihpatga@gmail.com>
2024-06-28 17:31:18 +0700

    build(file-config): next.config add key remove-console
    deskripsi:
    * menambah key remove-console: untuk setting hapus console saat di mode production, tapi console dengan tipe error tidak dihapus, karena sepertinya ini di perlukan di production

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ['lucide-react'],
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
+    },
 };
 
 export default nextConfig;

547c8f0dc08713f9c2215df5d8d8cb43de9ba9ae
Gigih Patga <gigihpatga@gmail.com>
2025-01-22 09:54:41 +0700

    feat(component): loading elipse icon
    description:
    * component icon for loading button
    * the svg element in the icon has animate tag. so, didn't additional css is needed to animate the icon loading.

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,195 @@
+import { cn } from '@/lib/classnames';
+import React, { type SVGProps } from 'react';
+
+export const LoadingElipseIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={200}
+        height={200}
+        preserveAspectRatio="xMidYMid"
+        className={cn('size-6 fill-gray-800 _bg-yellow-600', className)}
+        style={{
+            shapeRendering: 'auto',
+            display: 'block',
+            // background: '0 0',
+        }}
+        viewBox="0 0 100 100"
+        {...props}
+    >
+        <rect
+            // width={6}
+            width={8}
+            // height={14}
+            height={26}
+            x={47}
+            // y={25}
+            y={6}
+            // fill="#fe718d"
+            rx={3}
+            ry={3.64}
+        >
+            <animate
+                attributeName="opacity"
+                begin="-0.875s"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                values="1;0"
+            />
+        </rect>
+        <rect
+            // width={6}
+            width={8}
+            // height={14}
+            height={26}
+            x={47}
+            // y={25}
+            y={6}
+            // fill="#fe718d"
+            rx={3}
+            ry={3.64}
+            transform="rotate(45 50 50)"
+        >
+            <animate
+                attributeName="opacity"
+                begin="-0.75s"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                values="1;0"
+            />
+        </rect>
+        <rect
+            // width={6}
+            width={8}
+            // height={14}
+            height={26}
+            x={47}
+            // y={25}
+            y={6}
+            // fill="#fe718d"
+            rx={3}
+            ry={3.64}
+            transform="rotate(90 50 50)"
+        >
+            <animate
+                attributeName="opacity"
+                begin="-0.625s"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                values="1;0"
+            />
+        </rect>
+        <rect
+            // width={6}
+            width={8}
+            // height={14}
+            height={26}
+            x={47}
+            // y={25}
+            y={6}
+            // fill="#fe718d"
+            rx={3}
+            ry={3.64}
+            transform="rotate(135 50 50)"
+        >
+            <animate
+                attributeName="opacity"
+                begin="-0.5s"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                values="1;0"
+            />
+        </rect>
+        <rect
+            // width={6}
+            width={8}
+            // height={14}
+            height={26}
+            x={47}
+            // y={25}
+            y={6}
+            // fill="#fe718d"
+            rx={3}
+            ry={3.64}
+            transform="rotate(180 50 50)"
+        >
+            <animate
+                attributeName="opacity"
+                begin="-0.375s"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                values="1;0"
+            />
+        </rect>
+        <rect
+            // width={6}
+            width={8}
+            // height={14}
+            height={26}
+            x={47}
+            // y={25}
+            y={6}
+            // fill="#fe718d"
+            rx={3}
+            ry={3.64}
+            transform="rotate(225 50 50)"
+        >
+            <animate
+                attributeName="opacity"
+                begin="-0.25s"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                values="1;0"
+            />
+        </rect>
+        <rect
+            // width={6}
+            width={8}
+            // height={14}
+            height={26}
+            x={47}
+            // y={25}
+            y={6}
+            // fill="#fe718d"
+            rx={3}
+            ry={3.64}
+            transform="rotate(270 50 50)"
+        >
+            <animate
+                attributeName="opacity"
+                begin="-0.125s"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                values="1;0"
+            />
+        </rect>
+        <rect
+            // width={6}
+            width={8}
+            // height={14}
+            height={26}
+            x={47}
+            // y={25}
+            y={6}
+            // fill="#fe718d"
+            rx={3}
+            ry={3.64}
+            transform="rotate(315 50 50)"
+        >
+            <animate
+                attributeName="opacity"
+                begin="0s"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                values="1;0"
+            />
+        </rect>
+    </svg>
+);
---
+++ src/stories/components/button-loading.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useState } from 'react';
+import { UploadIcon } from 'lucide-react';
+
+import { LoadingElipseIcon } from '@/components/icons';
+import { Button } from '@/components/ui/button';
+
+export function ButtonLoading() {
+    const [isLoading, setIsLoading] = useState(false);
+
+    return (
+        <Button
+            className="size-7 _flex-shrink-0 text-xs p-0 flex"
+            onClick={() => void console.log('halo')}
+        >
+            {isLoading === false ? <LoadingElipseIcon className="size-4 fill-white" /> : <UploadIcon className="size-3 " />}
+        </Button>
+    );
+}

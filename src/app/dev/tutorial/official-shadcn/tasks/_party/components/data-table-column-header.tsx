f77b0733d4a2d81788201bbeabf75311b8275160
Gigih Patga <gigihpatga@gmail.com>
2025-01-28 06:03:04 +0700

    feat(dev-research): #with: official shadcn, taks for example of datatable using tanstack react table
    description:
    * integration component table shadcn with tanstack react table
    
    * dependencies:
    - @tanstack/react-table

---
+++ src/app/dev/tutorial/official-shadcn/tasks/_party/components/data-table-column-header.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Column } from '@tanstack/react-table';
+import { ArrowDown, ArrowUp, ChevronsUpDown, EyeOff } from 'lucide-react';
+
+import { cn } from '@/lib/classnames';
+import { Button } from '@/components/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+
+interface DataTableColumnHeaderProps<TData, TValue> extends React.HTMLAttributes<HTMLDivElement> {
+    column: Column<TData, TValue>;
+    title: string;
+}
+
+export function DataTableColumnHeader<TData, TValue>({ column, title, className }: DataTableColumnHeaderProps<TData, TValue>) {
+    if (!column.getCanSort()) {
+        return <div className={cn(className)}>{title}</div>;
+    }
+
+    return (
+        <div className={cn('flex items-center space-x-2', className)}>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="ghost"
+                        size="sm"
+                        className="_-ml-3 px-1 w-full _justify-start justify-between h-8 data-[state=open]:bg-accent [&_svg]:size-4 [&_svg]:shrink-0 space-x-2 gap-2"
+                    >
+                        <span>{title}</span>
+                        {column.getIsSorted() === 'desc' ? <ArrowDown /> : column.getIsSorted() === 'asc' ? <ArrowUp /> : <ChevronsUpDown />}
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="start">
+                    <DropdownMenuItem
+                        className="gap-2"
+                        onClick={() => column.toggleSorting(false)}
+                    >
+                        <ArrowUp className="h-3.5 w-3.5 text-muted-foreground/70" />
+                        Asc
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        className="gap-2"
+                        onClick={() => column.toggleSorting(true)}
+                    >
+                        <ArrowDown className="h-3.5 w-3.5 text-muted-foreground/70" />
+                        Desc
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                        className="gap-2"
+                        onClick={() => column.toggleVisibility(false)}
+                    >
+                        <EyeOff className="h-3.5 w-3.5 text-muted-foreground/70" />
+                        Hide
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    );
+}
